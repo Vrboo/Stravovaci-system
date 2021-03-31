115526b9940b863be36aeb001cd8f25eb08dbe8c
Vrboo <dominikvrbo@gmail.com>
2021-03-31 19:02:56 +0200

    konstruktor getteri setteri

--- src/main/java/sk/dominikvrbovsky/User.java
+++ src/main/java/sk/dominikvrbovsky/User.java
@@ -3,9 +3,46 @@ package sk.dominikvrbovsky;
 public class User {
 
     private String username;
-    private String fullname;
+    private String fullName;
     private String password;
     private double account;
 
+    public User(String username, String fullName, String password, double account) {
+        this.username = username;
+        this.fullName = fullName;
+        this.password = password;
+        this.account = account;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getAccount() {
+        return account;
+    }
+
+    public void setAccount(double account) {
+        this.account = account;
+    }
 }
