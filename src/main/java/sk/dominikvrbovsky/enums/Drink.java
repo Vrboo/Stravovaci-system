4def0687215af200c0984a673c14ab5c585d9153
Vrboo <dominikvrbo@gmail.com>
2021-04-01 11:49:22 +0200

    enum Drink is done

--- src/main/java/sk/dominikvrbovsky/enums/Drink.java
+++ src/main/java/sk/dominikvrbovsky/enums/Drink.java
@@ -1,5 +1,23 @@
 package sk.dominikvrbovsky.enums;
 
 public enum Drink {
+    TEA("caj"),
+    MINERAL_WATER("mineralna voda"),
+    JUICE("dzus"),
+    COLA("cola");
 
+    private final String drink;
+
+    Drink(String drink) {
+        this.drink = drink;
+    }
+
+    public String getDrink() {
+        return this.drink;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDrink();
+    }
 }
