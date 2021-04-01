e6383ff07b8bb9299bc9c49413dd65f4ac47ba3e
Vrboo <dominikvrbo@gmail.com>
2021-04-01 12:12:44 +0200

    Breakfast atribut, konstruktor, getteri setteri

--- src/main/java/sk/dominikvrbovsky/Breakfast.java
+++ src/main/java/sk/dominikvrbovsky/Breakfast.java
@@ -1,8 +1,28 @@
 package sk.dominikvrbovsky;
 
-public class Breakfast {
+import sk.dominikvrbovsky.enums.Drink;
 
+public class Breakfast extends Meal {
 
+    private Drink drink;
+
+    public Breakfast(String name, double price, int capacity, Drink drink) {
+        super(name, price, capacity);
+        this.drink = drink;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+
+    @Override
+    public String toStringMealMenu() {
+        return super.toStringMealMenu() + String.format("[%s]" , this.drink.toString());
+    }
 
 
 }
