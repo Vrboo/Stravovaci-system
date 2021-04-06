70fd49095206777efe745eb2af7e49d70aaf566f
Vrboo <dominikvrbo@gmail.com>
2021-04-06 07:37:09 +0200

    toString metody v Lunch

---
+++ src/main/java/sk/dominikvrbovsky/Lunch.java
@@ -0,0 +1,34 @@
+package sk.dominikvrbovsky;
+
+public class Lunch extends Meal{
+
+    private boolean takeaway;
+
+    public Lunch(String name, double price, int capacity, boolean takeaway) {
+        super(name, price, capacity);
+        this.takeaway = takeaway;
+    }
+
+    public boolean isTakeaway() {
+        return takeaway;
+    }
+
+    public void setTakeaway(boolean takeaway) {
+        this.takeaway = takeaway;
+    }
+
+    @Override
+    public String toStringMealMenu() {
+        return String.format("%s %s - %.2f€ - %dx ", getName(), this.takeaway ? "[Takeaway]":"", getPrice(), getCapacity());
+    }
+
+    @Override
+    public String toStringNumOfOrders() {
+        return String.format("%s %s - pocet objednavok %dx ", getName(), this.takeaway ? "[Takeaway]":"", getNumberOfOrder());
+    }
+
+    @Override
+    public String toStringBurza() {
+        return String.format("%s %s - %.2f€ ", getName(), this.takeaway ? "[Takeaway]":"", getPrice());
+    }
+}
