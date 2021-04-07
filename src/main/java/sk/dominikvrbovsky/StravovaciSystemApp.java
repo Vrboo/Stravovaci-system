def9398745a309439a48d8bfd71fc0a8e7f54117
Vrboo <dominikvrbo@gmail.com>
2021-04-07 12:57:39 +0200

    branch is done

--- src/main/java/sk/dominikvrbovsky/StravovaciSystemApp.java
+++ src/main/java/sk/dominikvrbovsky/StravovaciSystemApp.java
@@ -14,33 +14,7 @@ public class StravovaciSystemApp {
                 Persistence.createEntityManagerFactory("sk.dominikvrbovsky.stravovaci-system");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-//        Meal breakfast1 = new Breakfast("Palacinky", 12.50, 70, Drink.COLA);
-//        Meal breakfast2 = new Breakfast("Prazenica", 45, 30, Drink.TEA);
-//        Meal breakfast3 = new Breakfast("Parky", 5, 100, Drink.JUICE);
-//        Meal breakfast4 = new Breakfast("Croasant", 10.37, 5, Drink.MINERAL_WATER);
-//
-//        Meal lunch1 = new Lunch("Rezen", 12.30, 150, true);
-//        Meal lunch2 = new Lunch("Spagety", 5.21, 65, false);
-        Meal lunch3 = new Lunch("Cestoviny", 4.21, 54, false);
-        Meal lunch4 = new Lunch("Pizza", 8.45, 87, true);
 
-        entityManager.getTransaction().begin();
-
-//        entityManager.persist(lunch1);
-//        entityManager.persist(breakfast1);
-//        entityManager.persist(lunch4);
-//        entityManager.persist(breakfast2);
-//        entityManager.persist(breakfast3);
-//        entityManager.persist(lunch2);
-//        entityManager.persist(breakfast4);
-//        entityManager.persist(lunch3);
-
-        Meal lunchch = entityManager.find(Meal.class, 9L);
-        entityManager.remove(lunchch);
-
-
-
-        entityManager.getTransaction().commit();
 
         entityManager.close();
 
