8f8c86c39238a5d603a9aa37cf57c4fdb1168606
Vrboo <dominikvrbo@gmail.com>
2021-04-14 09:05:44 +0200

    az teraz pridana kniznica do projektu KControls a vytv. Login gui

---
+++ src/main/java/sk/dominikvrbovsky/gui/Login.java
@@ -0,0 +1,43 @@
+/*
+ * Created by JFormDesigner on Wed Apr 14 09:01:37 CEST 2021
+ */
+
+package sk.dominikvrbovsky.gui;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author Dominik Vrbovsky
+ */
+public class Login extends JFrame {
+    public Login() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Dominik Vrbovsky
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Dominik Vrbovsky
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
