68539fb4f7ec45cb5b5ee12454e9d32c3cd3b13b
Vrboo <dominikvrbo@gmail.com>
2021-04-15 17:39:58 +0200

    bordre na fieldoch funguju na 2px.

--- src/main/java/sk/dominikvrbovsky/gui/Registration.java
+++ src/main/java/sk/dominikvrbovsky/gui/Registration.java
@@ -145,12 +145,13 @@ public class Registration extends JFrame {
             panelBackRegistration.setBorder(null);
             panelBackRegistration.setBackground(new Color(0, 164, 210));
             panelBackRegistration.setkBorderRadius(0);
-            panelBackRegistration.setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing.
-            border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JFor\u006dDesi\u0067ner \u0045valu\u0061tion" , javax. swing .border . TitledBorder. CENTER
-            ,javax . swing. border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font
-            . BOLD ,12 ) ,java . awt. Color .red ) ,panelBackRegistration. getBorder () ) ); panelBackRegistration. addPropertyChangeListener(
-            new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "bord\u0065r"
-            .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+            panelBackRegistration.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.
+            swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion",javax.swing.border
+            .TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("D\u0069alog"
+            ,java.awt.Font.BOLD,12),java.awt.Color.red),panelBackRegistration. getBorder
+            ()));panelBackRegistration. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java
+            .beans.PropertyChangeEvent e){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException
+            ();}});
 
             //---- labelX ----
             labelX.setIcon(new ImageIcon(getClass().getResource("/icons/icons8_x_18px.png")));
@@ -183,13 +184,14 @@ public class Registration extends JFrame {
                 labelIcon.setIcon(new ImageIcon("C:\\Learn2Code\\MyApps\\stravovaci-system-2\\src\\main\\resources\\icons\\icons8_add_user_male_100px.png"));
 
                 //---- fieldPouzMeno ----
-                fieldPouzMeno.setBorder(new MatteBorder(0, 0, 1, 0, Color.black));
+                fieldPouzMeno.setBorder(new MatteBorder(0, 0, 2, 0, Color.black));
                 fieldPouzMeno.setText("Pou\u017e\u00edvate\u013esk\u00e9 meno");
                 fieldPouzMeno.setFocusable(false);
                 fieldPouzMeno.setHorizontalAlignment(SwingConstants.CENTER);
                 fieldPouzMeno.setFont(new Font("Yu Gothic UI", Font.BOLD, 17));
                 fieldPouzMeno.setForeground(Color.lightGray);
                 fieldPouzMeno.setPreferredSize(new Dimension(49, 29));
+                fieldPouzMeno.setCaretPosition(15);
                 fieldPouzMeno.addMouseMotionListener(new MouseMotionAdapter() {
                     @Override
                     public void mouseMoved(MouseEvent e) {
@@ -200,6 +202,7 @@ public class Registration extends JFrame {
                     @Override
                     public void focusGained(FocusEvent e) {
                         fieldPouzMenoFocusGained();
+                        fieldPouzMenoFocusGained();
                     }
                     @Override
                     public void focusLost(FocusEvent e) {
@@ -209,12 +212,13 @@ public class Registration extends JFrame {
 
                 //---- fielCeleMeno ----
                 fielCeleMeno.setText("Cel\u00e9 meno");
-                fielCeleMeno.setBorder(new MatteBorder(0, 0, 1, 0, Color.black));
+                fielCeleMeno.setBorder(new MatteBorder(0, 0, 2, 0, Color.black));
                 fielCeleMeno.setFocusable(false);
                 fielCeleMeno.setHorizontalAlignment(SwingConstants.CENTER);
                 fielCeleMeno.setFont(new Font("Yu Gothic UI", Font.BOLD, 17));
                 fielCeleMeno.setForeground(Color.lightGray);
                 fielCeleMeno.setPreferredSize(new Dimension(49, 29));
+                fielCeleMeno.setCaretPosition(9);
                 fielCeleMeno.addMouseMotionListener(new MouseMotionAdapter() {
                     @Override
                     public void mouseMoved(MouseEvent e) {
@@ -234,12 +238,13 @@ public class Registration extends JFrame {
                 });
 
                 //---- password1 ----
-                password1.setBorder(new MatteBorder(0, 0, 1, 0, Color.black));
+                password1.setBorder(new MatteBorder(0, 0, 2, 0, Color.black));
                 password1.setText("Heslo");
                 password1.setFocusable(false);
                 password1.setHorizontalAlignment(SwingConstants.CENTER);
                 password1.setFont(new Font("Yu Gothic UI", Font.BOLD, 17));
                 password1.setForeground(Color.lightGray);
+                password1.setCaretPosition(5);
                 password1.addMouseMotionListener(new MouseMotionAdapter() {
                     @Override
                     public void mouseMoved(MouseEvent e) {
@@ -260,13 +265,14 @@ public class Registration extends JFrame {
                 });
 
                 //---- password2 ----
-                password2.setBorder(new MatteBorder(0, 0, 1, 0, Color.black));
+                password2.setBorder(new MatteBorder(0, 0, 2, 0, Color.black));
                 password2.setText("Potvrdenie hesla");
                 password2.setFocusable(false);
                 password2.setHorizontalAlignment(SwingConstants.CENTER);
                 password2.setFont(new Font("Yu Gothic UI", Font.BOLD, 17));
                 password2.setForeground(Color.lightGray);
                 password2.setPreferredSize(new Dimension(49, 29));
+                password2.setCaretPosition(16);
                 password2.addMouseMotionListener(new MouseMotionAdapter() {
                     @Override
                     public void mouseMoved(MouseEvent e) {
@@ -307,19 +313,20 @@ public class Registration extends JFrame {
                 panelRegistrationLayout.setHorizontalGroup(
                     panelRegistrationLayout.createParallelGroup()
                         .addGroup(panelRegistrationLayout.createSequentialGroup()
-                            .addGap(23, 23, 23)
+                            .addContainerGap(23, Short.MAX_VALUE)
                             .addGroup(panelRegistrationLayout.createParallelGroup()
+                                .addGroup(panelRegistrationLayout.createSequentialGroup()
+                                    .addGroup(panelRegistrationLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                        .addComponent(buttonPrihlasit, GroupLayout.PREFERRED_SIZE, 249, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(password2, GroupLayout.PREFERRED_SIZE, 249, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(password1, GroupLayout.PREFERRED_SIZE, 249, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(fieldPouzMeno, GroupLayout.PREFERRED_SIZE, 249, GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(fielCeleMeno, GroupLayout.PREFERRED_SIZE, 249, GroupLayout.PREFERRED_SIZE))
+                                    .addGap(0, 20, Short.MAX_VALUE))
                                 .addGroup(GroupLayout.Alignment.TRAILING, panelRegistrationLayout.createSequentialGroup()
+                                    .addGap(0, 0, Short.MAX_VALUE)
                                     .addComponent(labelIcon)
-                                    .addGap(96, 96, 96))
-                                .addGroup(GroupLayout.Alignment.TRAILING, panelRegistrationLayout.createSequentialGroup()
-                                    .addGroup(panelRegistrationLayout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
-                                        .addComponent(fieldPouzMeno, GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
-                                        .addComponent(fielCeleMeno, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(password1)
-                                        .addComponent(buttonPrihlasit, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
-                                        .addComponent(password2, GroupLayout.Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE))
-                                    .addGap(20, 20, 20))))
+                                    .addGap(96, 96, 96))))
                 );
                 panelRegistrationLayout.setVerticalGroup(
                     panelRegistrationLayout.createParallelGroup()
@@ -328,15 +335,15 @@ public class Registration extends JFrame {
                             .addComponent(labelIcon)
                             .addGap(18, 18, 18)
                             .addComponent(fieldPouzMeno, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-                            .addGap(12, 12, 12)
+                            .addGap(18, 18, 18)
                             .addComponent(fielCeleMeno, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-                            .addGap(10, 10, 10)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 20, Short.MAX_VALUE)
                             .addComponent(password1, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addGap(18, 18, 18)
                             .addComponent(password2, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
                             .addGap(18, 18, 18)
                             .addComponent(buttonPrihlasit, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-                            .addContainerGap())
+                            .addGap(18, 18, 18))
                 );
             }
 
@@ -345,20 +352,20 @@ public class Registration extends JFrame {
             panelBackRegistrationLayout.setHorizontalGroup(
                 panelBackRegistrationLayout.createParallelGroup()
                     .addGroup(panelBackRegistrationLayout.createSequentialGroup()
-                        .addContainerGap(760, Short.MAX_VALUE)
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(labelX))
-                    .addGroup(GroupLayout.Alignment.TRAILING, panelBackRegistrationLayout.createSequentialGroup()
+                    .addGroup(panelBackRegistrationLayout.createSequentialGroup()
                         .addContainerGap(243, Short.MAX_VALUE)
                         .addComponent(panelRegistration, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 243, Short.MAX_VALUE))
+                        .addContainerGap(243, Short.MAX_VALUE))
             );
             panelBackRegistrationLayout.setVerticalGroup(
                 panelBackRegistrationLayout.createParallelGroup()
                     .addGroup(panelBackRegistrationLayout.createSequentialGroup()
                         .addComponent(labelX)
-                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(panelRegistration, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(33, Short.MAX_VALUE))
+                        .addContainerGap(41, Short.MAX_VALUE))
             );
         }
 
