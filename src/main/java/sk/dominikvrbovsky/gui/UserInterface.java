4a63999f9fcbd1d8db3fc5306a0d9aaf9f2cc71e
Vrboo <dominikvrbo@gmail.com>
2021-04-20 17:35:56 +0200

    pripravene na tvorbu contentu hlavneho menu

--- src/main/java/sk/dominikvrbovsky/gui/UserInterface.java
+++ src/main/java/sk/dominikvrbovsky/gui/UserInterface.java
@@ -4,13 +4,17 @@
 
 package sk.dominikvrbovsky.gui;
 
+import java.awt.event.*;
 import keeptoo.*;
 import sk.dominikvrbovsky.User;
+import sk.dominikvrbovsky.utilities.Utilities;
 
 import javax.persistence.EntityManager;
 import javax.swing.*;
 import javax.swing.GroupLayout;
 import java.awt.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Dominik Vrbovsky
@@ -24,6 +28,7 @@ public class UserInterface extends JFrame {
     public UserInterface(EntityManager entityManager, User user) {
         this.entityManager = entityManager;
         this.user = user;
+
         String userAccount = String.format("%.2f", user.getAccount());
 
         this.setPreferredSize(new Dimension(1000, 600));
@@ -35,7 +40,8 @@ public class UserInterface extends JFrame {
         
         labelUsername.setText(user.getFullName());
         labelAccount.setText("Stav účtu: " + userAccount + "€");
-
+        this.labelDatum.setText(Utilities.buildSlovakTimeString());
+        
         btnObjednat.setSelected(true);
 
     }
@@ -75,6 +81,20 @@ public class UserInterface extends JFrame {
     private void btnRanajkyActionPerformed() {
         cardLayoutObjednat.show(panelObjednatContent, "ranajky");
     }
+
+    private void labelXObjednatMouseEntered() {
+        labelXObjednat.setIcon(
+                new ImageIcon("C:\\Learn2Code\\MyApps\\stravovaci-system-2\\src\\main\\resources\\icons\\icons8_x_18px_6.png"));
+    }
+
+    private void labelXObjednatMouseExited() {
+        labelXObjednat.setIcon(new 
+                ImageIcon("C:\\Learn2Code\\MyApps\\stravovaci-system-2\\src\\main\\resources\\icons\\icons8_x_18px.png"));
+    }
+
+    private void labelXObjednatMouseClicked() {
+        System.exit(0);
+    }
     
 
 //    private void btnObedActionPerformed() {
@@ -104,8 +124,9 @@ public class UserInterface extends JFrame {
         panelObjednat = new KGradientPanel();
         splitPane2 = new JSplitPane();
         panelObjednatMenu = new KGradientPanel();
-        label1 = new JLabel();
-        label8 = new JLabel();
+        labelStravovaciSystem = new JLabel();
+        labelDatum = new JLabel();
+        labelXObjednat = new JLabel();
         panelObjednatContent = new KGradientPanel();
         panelObjednatObed = new KGradientPanel();
         panelObjednatRanajky = new KGradientPanel();
@@ -139,13 +160,11 @@ public class UserInterface extends JFrame {
                 panelMenu.setkBorderRadius(0);
                 panelMenu.setkStartColor(new Color(55, 55, 55));
                 panelMenu.setkEndColor(new Color(55, 55, 55));
-                panelMenu.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax
-                .swing.border.EmptyBorder(0,0,0,0), "JFor\u006dDesi\u0067ner \u0045valu\u0061tion",javax.swing
-                .border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.
-                Font("Dia\u006cog",java.awt.Font.BOLD,12),java.awt.Color.red
-                ),panelMenu. getBorder()));panelMenu. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override
-                public void propertyChange(java.beans.PropertyChangeEvent e){if("bord\u0065r".equals(e.getPropertyName(
-                )))throw new RuntimeException();}});
+                panelMenu.setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder(
+                0, 0, 0, 0) , "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax. swing. border. TitledBorder. CENTER, javax. swing. border. TitledBorder
+                . BOTTOM, new java .awt .Font ("Dia\u006cog" ,java .awt .Font .BOLD ,12 ), java. awt. Color.
+                red) ,panelMenu. getBorder( )) ); panelMenu. addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .
+                beans .PropertyChangeEvent e) {if ("\u0062ord\u0065r" .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
 
                 //---- labelIcon ----
                 labelIcon.setHorizontalAlignment(SwingConstants.CENTER);
@@ -354,37 +373,60 @@ public class UserInterface extends JFrame {
                             panelObjednatMenu.setkEndColor(new Color(150, 223, 141));
                             panelObjednatMenu.setkGradientFocus(600);
 
-                            //---- label1 ----
-                            label1.setText("Stravovac\u00ed syst\u00e9m");
-                            label1.setFont(new Font("Yu Gothic UI", Font.BOLD, 45));
-                            label1.setHorizontalAlignment(SwingConstants.LEFT);
-                            label1.setForeground(new Color(70, 70, 70));
+                            //---- labelStravovaciSystem ----
+                            labelStravovaciSystem.setText("Stravovac\u00ed syst\u00e9m");
+                            labelStravovaciSystem.setFont(new Font("Yu Gothic UI", Font.BOLD, 45));
+                            labelStravovaciSystem.setHorizontalAlignment(SwingConstants.LEFT);
+                            labelStravovaciSystem.setForeground(new Color(70, 70, 70));
 
-                            //---- label8 ----
-                            label8.setText("20. janu\u00e1r 2021, utorok");
-                            label8.setFont(new Font("Yu Gothic UI", Font.BOLD, 19));
-                            label8.setHorizontalAlignment(SwingConstants.LEFT);
-                            label8.setForeground(new Color(70, 70, 70));
+                            //---- labelDatum ----
+                            labelDatum.setText("Utorok, 20. apr\u00edl 2021");
+                            labelDatum.setFont(new Font("Yu Gothic UI", Font.BOLD, 20));
+                            labelDatum.setForeground(new Color(70, 70, 70));
+                            labelDatum.setHorizontalAlignment(SwingConstants.LEFT);
+
+                            //---- labelXObjednat ----
+                            labelXObjednat.setIcon(new ImageIcon("C:\\Learn2Code\\MyApps\\stravovaci-system-2\\src\\main\\resources\\icons\\icons8_x_18px.png"));
+                            labelXObjednat.setHorizontalAlignment(SwingConstants.CENTER);
+                            labelXObjednat.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                            labelXObjednat.addMouseListener(new MouseAdapter() {
+                                @Override
+                                public void mouseClicked(MouseEvent e) {
+                                    labelXObjednatMouseClicked();
+                                }
+                                @Override
+                                public void mouseEntered(MouseEvent e) {
+                                    labelXObjednatMouseEntered();
+                                }
+                                @Override
+                                public void mouseExited(MouseEvent e) {
+                                    labelXObjednatMouseExited();
+                                }
+                            });
 
                             GroupLayout panelObjednatMenuLayout = new GroupLayout(panelObjednatMenu);
                             panelObjednatMenu.setLayout(panelObjednatMenuLayout);
                             panelObjednatMenuLayout.setHorizontalGroup(
                                 panelObjednatMenuLayout.createParallelGroup()
                                     .addGroup(panelObjednatMenuLayout.createSequentialGroup()
-                                        .addGap(36, 36, 36)
-                                        .addGroup(panelObjednatMenuLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-                                            .addComponent(label1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(label8, GroupLayout.PREFERRED_SIZE, 378, GroupLayout.PREFERRED_SIZE))
-                                        .addContainerGap(194, Short.MAX_VALUE))
+                                        .addGap(35, 35, 35)
+                                        .addGroup(panelObjednatMenuLayout.createParallelGroup()
+                                            .addComponent(labelStravovaciSystem, GroupLayout.PREFERRED_SIZE, 378, GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(labelDatum, GroupLayout.PREFERRED_SIZE, 378, GroupLayout.PREFERRED_SIZE))
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 168, Short.MAX_VALUE)
+                                        .addComponent(labelXObjednat, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE))
                             );
                             panelObjednatMenuLayout.setVerticalGroup(
                                 panelObjednatMenuLayout.createParallelGroup()
                                     .addGroup(panelObjednatMenuLayout.createSequentialGroup()
-                                        .addGap(16, 16, 16)
-                                        .addComponent(label1, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(panelObjednatMenuLayout.createParallelGroup()
+                                            .addComponent(labelXObjednat, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(panelObjednatMenuLayout.createSequentialGroup()
+                                                .addGap(14, 14, 14)
+                                                .addComponent(labelStravovaciSystem, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)))
                                         .addGap(0, 0, 0)
-                                        .addComponent(label8, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-                                        .addContainerGap(53, Short.MAX_VALUE))
+                                        .addComponent(labelDatum, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+                                        .addContainerGap(60, Short.MAX_VALUE))
                             );
                         }
                         splitPane2.setTopComponent(panelObjednatMenu);
@@ -406,7 +448,7 @@ public class UserInterface extends JFrame {
                                 );
                                 panelObjednatObedLayout.setVerticalGroup(
                                     panelObjednatObedLayout.createParallelGroup()
-                                        .addGap(0, 368, Short.MAX_VALUE)
+                                        .addGap(0, 363, Short.MAX_VALUE)
                                 );
                             }
                             panelObjednatContent.add(panelObjednatObed, "obed");
@@ -426,7 +468,7 @@ public class UserInterface extends JFrame {
                                 );
                                 panelObjednatRanajkyLayout.setVerticalGroup(
                                     panelObjednatRanajkyLayout.createParallelGroup()
-                                        .addGap(0, 368, Short.MAX_VALUE)
+                                        .addGap(0, 363, Short.MAX_VALUE)
                                 );
                             }
                             panelObjednatContent.add(panelObjednatRanajky, "ranajky");
@@ -635,8 +677,9 @@ public class UserInterface extends JFrame {
     private KGradientPanel panelObjednat;
     private JSplitPane splitPane2;
     private KGradientPanel panelObjednatMenu;
-    private JLabel label1;
-    private JLabel label8;
+    private JLabel labelStravovaciSystem;
+    private JLabel labelDatum;
+    private JLabel labelXObjednat;
     private KGradientPanel panelObjednatContent;
     private KGradientPanel panelObjednatObed;
     private KGradientPanel panelObjednatRanajky;
--- src/main/java/sk/dominikvrbovsky/utilities/Utilities.java
+++ src/main/java/sk/dominikvrbovsky/utilities/Utilities.java
@@ -1,5 +1,6 @@
 package sk.dominikvrbovsky.utilities;
 
+import java.time.LocalDate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,4 +12,49 @@ public class Utilities {
 
         return matcher.matches();
     }
+
+    public static String buildSlovakTimeString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        LocalDate localDate = LocalDate.now();
+        stringBuilder.append(getDayInSlovak(localDate.getDayOfWeek().name().toLowerCase()));
+        stringBuilder.append(", ").append(localDate.getDayOfMonth()).append(". ");
+        stringBuilder.append(getMonthInSlovak(localDate.getMonth().name().toLowerCase()));
+        stringBuilder.append(" ").append(localDate.getYear());
+
+        return stringBuilder.toString();
+    }
+
+    private static String getDayInSlovak(String dayInEnglish) {
+
+        return switch (dayInEnglish) {
+            case "monday" -> "Pondelok";
+            case "tuesday" -> "Utorok";
+            case "wednesday" -> "Streda";
+            case "thursday" -> "Štvrtok";
+            case "friday" -> "Piatok";
+            case "saturday" -> "Sobota";
+            case "sunday" -> "Nedeľa";
+            default -> null;
+        };
+    }
+
+    private static String getMonthInSlovak(String monthInEnglish) {
+
+        return switch (monthInEnglish) {
+            case "january" -> "január";
+            case "february" -> "február";
+            case "march" -> "marec";
+            case "april" -> "apríl";
+            case "may" -> "máj";
+            case "june" -> "jún";
+            case "july" -> "júl";
+            case "august" -> "august";
+            case "september" -> "september";
+            case "october" -> "október";
+            case "november" -> "november";
+            case "december" -> "decemeber";
+            default -> null;
+        };
+    }
 }
+
