fd60b91f64f74480ec09c2f6ee3ce2c4c4fd3f67
Vrboo <dominikvrbo@gmail.com>
2021-04-23 20:16:38 +0200

    vytvoril som JFrame AdministratorInterface

---
+++ src/main/java/sk/dominikvrbovsky/gui/AdministratorInterface.java
@@ -0,0 +1,43 @@
+/*
+ * Created by JFormDesigner on Fri Apr 23 20:16:11 CEST 2021
+ */
+
+package sk.dominikvrbovsky.gui;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author Dominik Vrbovsky
+ */
+public class AdministratorInterface extends JFrame {
+    public AdministratorInterface() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Dominik Vrbovsky
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 778, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 468, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Dominik Vrbovsky
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
